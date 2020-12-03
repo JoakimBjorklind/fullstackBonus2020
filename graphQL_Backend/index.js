03162b1bfa37d39157301714f53abb5619b2ebe3
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-12-03 21:31:12 +0200

    exercise 8.4 ok!

--- graphQL_Backend/index.js
+++ graphQL_Backend/index.js
@@ -101,7 +101,7 @@ const typeDefs = gql`
   type Query {
     bookCount: String!
     authorCount: String!
-    allBooks: [Book]!
+    allBooks(author: String): [Book]!
     allAuthors: [Author]!
   }
 `
@@ -110,7 +110,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if(!args.author) {
+       return books
+      }
+      return books.filter(boo => boo.author === args.author)
+      },
     allAuthors: () => authors
   },
   Author: {
